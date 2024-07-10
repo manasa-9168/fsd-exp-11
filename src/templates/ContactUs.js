e4f86a20e6831cbfd4a5b8e13f7c6769439441bb
manasa-9168 <163173068+manasa-9168@users.noreply.github.com>
2024-07-10 06:51:14 +0530

    Add files via upload

---
+++ src/templates/ContactUs.js
@@ -0,0 +1,5 @@
+const ContactUs=()=>{
+    return <h1>CMRIT, Kandlakoya Village, Medchal, Hyderabad</h1>;
+};
+
+export default ContactUs;
---
+++ src/templates/Profile.js
@@ -0,0 +1,6 @@
+const Profile=()=>{
+    return <><h1>Welcome ABC</h1>
+    <p>CSE-B</p></>
+};
+
+export default Profile;
